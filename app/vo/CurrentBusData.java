db7b8483fe7cb9f5217b4a23bfa27a2c4ad5be02
laiweiwei <l.weiwei@163.com>
2012-05-10 12:13:11 +0800

    add gps job to generate data

---
+++ app/vo/CurrentBusData.java
@@ -0,0 +1,21 @@
+package vo;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Wang
+ * Date: 3/5/12
+ * Time: 12:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CurrentBusData {
+    public int id;
+    public String busPlateNumber;
+    public String serviceNumber;
+    public String driver;
+    public int currentSpeed;
+    public double xCoord;
+    public double yCoord;
+    public String vehicleType;
+    public String activeStatus;
+    public String direction;
+}
